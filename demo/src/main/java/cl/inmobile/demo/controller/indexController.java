342cd544315e0c4e2b249df2daa06d24d65a9d25
Rodolfo Quezada <rodolfo.quezada.s@gmail.com>
2018-01-14 02:25:08 -0300

    solucion CSS
    asd

--- demo/src/main/java/cl/inmobile/demo/controller/indexController.java
+++ demo/src/main/java/cl/inmobile/demo/controller/indexController.java
@@ -5,10 +5,72 @@
  */
 package cl.inmobile.demo.controller;
 
+import cl.inmobile.demo.ejb.PersonaFacadeLocal;
+import cl.inmobile.demo.model.Persona;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
 /**
  *
  * @author rodo
  */
-public class indexController {
+@Named
+@ViewScoped
+public class indexController implements Serializable{
+
+    @EJB
+    private PersonaFacadeLocal personaFacade;
+    
+    private Persona persona;
+
+    private List<Persona> listaPersonas;
+    
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    
+    @PostConstruct
+    public void init(){
+      persona = new Persona();
+      listaPersonas = personaFacade.findAll();
+    }
+    
+    public String crear(){
+        try {
+            personaFacade.create(persona);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se a registrado de forma correcta "));
+        } catch (Exception e) {
+        }
+        return "buscarForm";
+    }
+    
+    public String eliminar(Persona per){
+        try {
+            personaFacade.remove(per);
+             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Aviso", "Se a eliminado de forma correcta "));
+        } catch (Exception e) {
+        }
+        return "buscarForm";
+    }
+
+    public List<Persona> getListaPersonas() {
+        return listaPersonas;
+    }
+
+    public void setListaPersonas(List<Persona> listaPersonas) {
+        this.listaPersonas = listaPersonas;
+    }
+    
     
 }
